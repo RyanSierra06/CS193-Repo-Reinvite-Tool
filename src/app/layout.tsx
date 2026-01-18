a381536d7fb738bbeeef374f5358dbab7f3d7b72
RyanSierra06 <rsierra@purdue.edu>
2026-01-18 17:48:43 -0500

    Deployment issue change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -53,14 +53,15 @@ export const metadata: Metadata = {
     creator: "@ygilany",
     images: ["/og-image.png"],
   },
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
-  },
+  category: "technology",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
   themeColor: "#8B0000",
   colorScheme: "light dark",
-  category: "technology",
 };
 
 export default function RootLayout({
